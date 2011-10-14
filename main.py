51a1deb15f334b217ebc055a69766cc306b8e0d0
Helmut Grohne <helmut@subdivi.de>
2011-10-15 00:01:34 +0200

    first attempt to handling cookies

--- main.py
+++ main.py
@@ -1,19 +1,60 @@
 #!/usr/bin/env python
 
+import Cookie
 import jinja2
+import random
 from wsgitools.applications import StaticContent
 from wsgitools.middlewares import TracebackMiddleware
 from wsgitools.scgi.asynchronous import SCGIServer
+
+sysrand = random.SystemRandom()
+
+class CookieHandler:
+    def __init__(self, name="sid", bits=64):
+        self.name = name
+        self.bits = bits
+
+    def get(self, environ):
+        cookiestr = environ.get("HTTP_COOKIE")
+        if not cookiestr:
+            return None
+        cookie = Cookie.SimpleCookie()
+        try:
+            cookie.load(cookiestr)
+        except Cookie.CookieError:
+            return None
+        try:
+            return cookie[self.name].value
+        except KeyError:
+            return None
+
+    def set(self, value):
+        cookiemorsel = Cookie.Morsel()
+        cookiemorsel.set(self.name, value, value)
+        return ("Set-Cookie", cookiemorsel.OutputString())
+
+    def newvalue(self):
+        return "%x" % random.getrandbits(self.bits)
+
+    def new(self):
+        return self.set(self.newvalue())
 
 class Application:
     def __init__(self):
         self.jinjaenv = jinja2.Environment(
                 loader=jinja2.FileSystemLoader("./templates"))
+        self.cookiehandler = CookieHandler()
     def __call__(self, environ, start_response):
+        headers = {
+            "Content-Type": "text/html; charset=utf8"
+        }
         content = self.jinjaenv.get_template("base.html").render({}) \
                   .encode("utf8")
+        cookie = self.cookiehandler.get(environ)
+        if cookie is None:
+            headers.__setitem__(*self.cookiehandler.new())
         return StaticContent("200 OK",
-                             [("Content-Type", "text/html; charset=utf8")],
+                             list(headers.items()),
                              content)(environ, start_response)
 
 def main():
