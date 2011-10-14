500c5cc591a83ba5b40239814fea4c334e01748b
Helmut Grohne <helmut@subdivi.de>
2011-10-14 22:53:53 +0200

    added first jinja template

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
+import jinja2
+from wsgitools.applications import StaticContent
+from wsgitools.middlewares import TracebackMiddleware
 from wsgitools.scgi.asynchronous import SCGIServer
 
-def app(environ, start_response):
-    start_response("200 OK", [])
-    return []
+class Application:
+    def __init__(self):
+        self.jinjaenv = jinja2.Environment(
+                loader=jinja2.FileSystemLoader("./templates"))
+    def __call__(self, environ, start_response):
+        content = self.jinjaenv.get_template("base.html").render({}) \
+                  .encode("utf8")
+        return StaticContent("200 OK",
+                             [("Content-Type", "text/html; charset=utf8")],
+                             content)(environ, start_response)
 
 def main():
+    app = Application()
+    app = TracebackMiddleware(app)
     server = SCGIServer(app, 4000)
     server.run()
 
