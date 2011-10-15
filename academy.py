15d7bc50053aa64d81c50ae00873bb3ccb4731c3
Markus Oehme <mo@wurzel.org>
2011-10-15 22:50:17 +0200

    even more docs

--- academy.py
+++ academy.py
@@ -8,7 +8,9 @@ from course import Course, CourseLite
 
 class AcademyLite:
     """
-    ...
+    Backend for viewing the file structres related to an academy
+
+    A detailed description can be found with the class Academy.
     """
 
     def __init__(self, obj):
@@ -22,12 +24,15 @@ class AcademyLite:
     @property
     def name(self):
         return os.path.basename(self.path)
+
     def gettitle(self):
         s=storage.Storage(self.path,"title")
         return s.content()
+
     def getgroups(self):
         s=storage.Storage(self.path,"groups")
         return s.content().split(' ')
+
     def listCoursesLite(self):
         candidates = os.listdir(self.path)
         final = copy.deepcopy(candidates)
@@ -35,6 +40,7 @@ class AcademyLite:
             if not os.path.isdir(os.path.join(self.path, x)):
                 final.remove(x)
         return [course.CourseLite(os.path.join(self.path, x)) for x in final]
+
     def getCourseLite(self,coursename):
         if re.match('^[-a-zA-Z0-9]{1,200}$', coursename) is None:
             return None
@@ -42,19 +48,36 @@ class AcademyLite:
         if not os.path.isdir(finalpath):
             return None
         return CourseLite(finalpath)
-        
+
 
 class Academy(AcademyLite):
+    """
+    Backend for manipulating the file structres related to a course
+
+    It is characterised by the path of a directory. The directory should
+    contain the following files. All directories within this directory are
+    assumed to contain a course.
+
+    title,v    The title of this display name of this academy
+    groups,v   The groups in which this academy is a member
+    """
     def __init__(self, obj):
         """
+        @param obj: either a path or an Academy object
+        @type obj: str or Academy object or AcademyLite object
         """
         AcademyLite.__init__(self, obj)
+
     def settitle(self, title):
         """
         Set the title of this academy
+
+        @param title: display name of the academy
+        @type title: str
         """
         s=storage.Storage(self.path,"title")
         s.store(title)
+
     def setgroups(self, groups):
         """
         Set the groups of this academy to determine when to display it
@@ -64,27 +87,42 @@ class Academy(AcademyLite):
         """
         s=storage.Storage(self.path,"groups")
         s.store(' '.join(x for x in groups))
+
     def createCourse(self, name, title):
         """
         create a new course
 
         @param name: internal name of the course
         @param title: displayed name of the course
-        @type name: str (restricted char set)
+        @type name: str (restricted char-set)
         @type title: str
         """
         if re.match('^[-a-zA-Z0-9]{1,200}$', name) is None:
             return False
         c = course.Course(os.path.join(self.path, name))
         c.settitle(title)
+
     def listCourses(self):
+        """
+        @returns: list of Course object; all courses of this academy
+        """
         candidates = os.listdir(self.path)
         final = copy.deepcopy(candidates)
         for x in candidates:
             if not os.isdir(os.path.join(self.path, x)):
                 final.remove(x)
         return [course.Course(os.path.join(self.path, x)) for x in final]
+
     def getCourse(self,coursename):
+        """
+        find a course of this academy to a given name
+
+        @param coursename: internal name of course
+        @type coursename: str (restricted char-set)
+        @returns: Course object for course with name coursename
+        """
         litecourse = self.getCourseLite(coursename)
+        if litecourse is None:
+            return None
         return Course(litecourse)
-        
+
