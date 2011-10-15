fb65c417b50baf8d6731f39c4a342684c54e3052
Markus Oehme <mo@wurzel.org>
2011-10-15 15:32:52 +0200

    updated script for example userdb

--- createuserdb.py
+++ createuserdb.py
@@ -5,9 +5,12 @@ import storage
 import user
 
 if __name__ == "__main__":
-    os.mkdir("work")
+    try:
+        os.mkdir("work")
+    except OSError:
+        pass
     mystore = storage.Storage('work', 'userdb')
     userdb = user.UserDB(mystore)
     userdb.addUser("arthur", "dokubeauftragter", "mypass", dict([("akademie_read_pa2010", True)]))
-    userdb.addUser("bob", "dokuteam", "secret", dict([("akademie_read_pa2010", True), ("akademie_write_pa2010", True)]))
+    userdb.addUser("bob", "dokuteam", "secret", dict([("akademie_read_pa2010", True), ("akademie_write_pa2010", True), ("df_useradmin", True)]))
     userdb.store()
