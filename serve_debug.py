5819c1a28fa26300821c4f71333dcc35db71aded
Helmut Grohne <helmut@subdivi.de>
2011-10-28 19:57:16 +0200

    switched debugger to werkzeug
    WARNING: serve_debug.py will enable unauthenticated shell access

---
+++ serve_debug.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""
+Simple server listening on http://localhost:8800/ with a traceback middleware
+and logging requests to stdout.
+
+WARNING: anyone producing an exception can execute any python code via http
+"""
+
+from wsgiref.simple_server import make_server
+
+from werkzeug.debug import DebuggedApplication
+
+from dokuforge import buildapp
+
+def main():
+    app = buildapp()
+    app = DebuggedApplication(app, evalex=True)
+    make_server("localhost", 8800, app).serve_forever()
+
+if __name__ == '__main__':
+    main()
