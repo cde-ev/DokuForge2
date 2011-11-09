2131043b4297110afbd42cefb28b1dcf46c56659
Klaus Aehlig <aehlig@linta.de>
2011-11-09 05:42:17 +0100

    use configfiles (step 1): take the port to listen from the config file

--- dokuforge/serve_scgi_forkpool.py
+++ dokuforge/serve_scgi_forkpool.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
-Forking server listening on scgi://localhost:4000/ with a spamassassin like
-worker model.
+Forking server listening on scgi://localhost with a spamassassin like
+worker model. The port to listen will be taken from the configuration file
 """
 
 from wsgitools.scgi.forkpool import SCGIServer
@@ -9,11 +9,22 @@ from wsgitools.scgi.forkpool import SCGIServer
 from dokuforge import buildapp
 from dokuforge.paths import PathConfig
 
-def main():
+import ConfigParser
+import sys
+
+def main(configfile):
+    config = ConfigParser.RawConfigParser()
+    config.read(configfile)
+
+    port = int(config.get('scgi','port'))
+
     pc = PathConfig()
     pc.sessiondbpath = "%(workdir)s/sessiondb.sqlite3"
     app = buildapp(pc)
-    SCGIServer(app, 4000).enable_sighandler().run()
+    SCGIServer(app, port).enable_sighandler().run()
 
 if __name__ == '__main__':
-    main()
+    configfile = "./dokuforge.conf.sample"
+    if len(sys.argv) > 1:
+        configfile = sys.argv[1]
+    main(configfile)
