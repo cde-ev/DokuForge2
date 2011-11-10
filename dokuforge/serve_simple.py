5a159e3123c07411324d88efba0dd0bbdce49b71
Helmut Grohne <helmut@subdivi.de>
2011-11-10 12:23:25 +0100

    support config file in dokuforge.serve_simple

--- dokuforge/serve_simple.py
+++ dokuforge/serve_simple.py
@@ -4,14 +4,23 @@ Simple server listening on http://localhost:8800/ with a traceback middleware
 and logging requests to stdout.
 """
 
+from ConfigParser import SafeConfigParser
+import sys
 from wsgiref.simple_server import make_server
 
 from wsgitools.middlewares import TracebackMiddleware
 
 from dokuforge import buildapp
+from dokuforge.paths import PathConfig
 
 def main():
-    app = buildapp()
+    configfile = "./dokuforge.conf.sample"
+    if len(sys.argv) > 1:
+        configfile = sys.argv[1]
+    config = SafeConfigParser()
+    config.read(configfile)
+    pc = PathConfig(config)
+    app = buildapp(pc)
     app = TracebackMiddleware(app)
     make_server("localhost", 8800, app).serve_forever()
 
