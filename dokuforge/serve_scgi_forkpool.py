92a52fb45a9de101033abc9f952829c5cf49ce00
Helmut Grohne <helmut@subdivi.de>
2011-11-13 18:19:07 +0100

    added ExceptionsToSyslog to serve_scgi_forkpool

--- dokuforge/serve_scgi_forkpool.py
+++ dokuforge/serve_scgi_forkpool.py
@@ -11,6 +11,25 @@ from dokuforge.paths import PathConfig
 
 import ConfigParser
 import sys
+import syslog
+import traceback
+
+def do_syslog(msg):
+    syslog.syslog(syslog.LOG_ERR | syslog.LOG_DAEMON | syslog.LOG_PID, msg)
+
+class ExceptionsToSyslog:
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        try:
+            return self.app(environ, start_response)
+        except:
+            exc_info = sys.exc_info()
+            do_syslog("Received Python exception: %s" % (exc_info[1],))
+            for line in traceback.format_exc(exc_info[2]).splitlines():
+                do_syslog(line)
+            raise # will get 503 from apache
 
 def main(configfile):
     config = ConfigParser.SafeConfigParser()
@@ -18,6 +37,7 @@ def main(configfile):
     port = int(config.get('scgi','port'))
     pc = PathConfig(config)
     app = buildapp(pc)
+    app = ExceptionsToSyslog(app)
     SCGIServer(app, port).enable_sighandler().run()
 
 if __name__ == '__main__':
