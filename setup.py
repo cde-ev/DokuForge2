7b327231dec47d24d3242bb47866860ed3c67233
Helmut Grohne <helmut@subdivi.de>
2011-11-15 09:28:55 +0100

    hack setup.py to include the git commit id
    setup.py will create dokuforge/versioninfo.py, call setup and remove the
    file. The file contains a single variable commitid containing the commit
    id. If the commit id could not be determined, the file will be absent,
    so users must catch ImportError.

--- setup.py
+++ setup.py
@@ -1,9 +1,34 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
+import subprocess
 
+def add_versioninfo():
+    try:
+        p = subprocess.Popen(["git", "show", "-s", "--format=%H"],
+                             stdout=subprocess.PIPE)
+    except OSError:
+        return
+    commitid, _ = p.communicate()
+    if p.returncode != 0:
+        return
+    commitid = commitid.strip()
+    with file(os.path.join(os.path.dirname(__file__), "dokuforge",
+                           "versioninfo.py"), "w") as verfile:
+        verfile.write('commitid = "%s"\n' % commitid)
+
+def clean_versioninfo():
+    try:
+        os.unlink(os.path.join(os.path.dirname(__file__), "dokuforge",
+                               "versioninfo.py"))
+    except OSError:
+        pass
+
+add_versioninfo()
 setup(name="dokuforge",
       packages=["dokuforge"],
       package_data=dict(dokuforge=["templates/*.html",
                                    "templates/style/*",
                                    "static/*.css"]))
+clean_versioninfo()
