a43558d84c5b4dc2252c755acc9593bce22f4a97
Markus Oehme <mo@wurzel.org>
2011-11-18 14:59:23 +0100

    Fold dokuforgeparser back into treeparser

--- dokuforge/treeparser.py
+++ dokuforge/treeparser.py
@@ -102,6 +102,19 @@ class TreeParser:
     @ivar stack: contains the current context
     @ivar output: is a stack of current outputs
     """
+    handle_heading = u"[%s]".__mod__
+    handle_subheading = u"[[%s]]".__mod__
+    handle_ednote = u"{%s}".__mod__
+    handle_displaymath = u"$$%1s$$".__mod__
+    handle_authors = u"(%s)".__mod__
+    handle_paragraph = u"%s".__mod__
+    handle_list = u"%s".__mod__
+    handle_item = u"- %s".__mod__
+    handle_emphasis = u"_%s_".__mod__
+    handle_keyword = u"*%s*".__mod__
+    handle_inlinemath = u"$%1s$".__mod__
+    handle_nestedednote = u"{%s}".__mod__
+
     def __init__(self, string, debug=False):
         assert isinstance(string, unicode)
         self.input = string
@@ -109,6 +122,7 @@ class TreeParser:
         self.pos = 0
         self.stack = [ "start" ]
         self.tree = ParseTree("root")
+        self.output = u""
 
     def lookstate(self):
         """
@@ -522,3 +536,24 @@ class TreeParser:
 
         ## finally return the tree
         return self.tree
+
+    def generateoutput(self, tree=None):
+        root = False
+        if tree is None:
+            tree = self.tree
+            root = True
+        if isinstance(tree, ParseLeaf):
+            return tree.data
+        output = u""
+        for x in tree.tree:
+            value = self.generateoutput(x)
+            try:
+                handler = getattr(self, "handle_%s" % x.ident)
+            except AttributeError:
+                pass
+            else:
+                value = handler(value)
+            output += value
+        if root:
+            self.output = output
+        return output
