153aa15a8aa88307eed137e23f5eca661596bff9
Markus Oehme <veelai.1764@googlemail.com>
2011-11-21 13:36:30 +0100

    Tune estimator.

--- dokuforge/estimatorparser.py
+++ dokuforge/estimatorparser.py
@@ -36,7 +36,11 @@ class Estimator(BaseParser):
     So certain structures which are known to cause a linebreak account at least
     a full line. The result of this parser is an int.
     """
-    linelength = 80
+    linelength = 85
+
+    def __init__(self, string, ednotes=False):
+        BaseParser.__init__(self, string)
+        self.ednotes = ednotes
 
     def fullline(self, line):
         """Return an estimate for the length of this line.
@@ -46,15 +50,35 @@ class Estimator(BaseParser):
         """
         return Estimate(max(len(line), self.linelength))
 
-    handle_heading = fullline
+    def twolines(self, line):
+        """Return an estimate for the length of this line.
+        @type line: unicode
+        @rtype: Estimate
+        @returns: an estimate of the length of the line, but at least two lines
+        """
+        return Estimate(max(len(line), 2*self.linelength))
+
+    def threelines(self, line):
+        """Return an estimate for the length of this line.
+        @type line: unicode
+        @rtype: Estimate
+        @returns: an estimate of the length of the line, but at least three lines
+        """
+        return Estimate(max(len(line), 3*self.linelength))
+
+
+    handle_heading = twolines
     handle_subheading = fullline
     handle_authors = fullline
     handle_item = fullline
-    handle_displaymath = fullline
+    handle_displaymath = threelines
 
     def handle_ednote(self, ednote):
-        """disccard ednotes"""
-        return Estimate(0)
+        """include ednotes according to self.ednotes"""
+        if self.ednotes:
+            return Estimate(ednote)
+        else:
+            return Estimate(0)
 
     def result(self):
         return int(Estimate(BaseParser.result(self)))
