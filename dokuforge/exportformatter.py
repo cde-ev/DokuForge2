eb63b4774d063006efbdfc3c6ac3f778fc3f5ad1
Markus Oehme <veelai.1764@googlemail.com>
2011-11-28 10:12:37 +0100

    Prepare exporterparser.
    This adds a load of information, which will be available at output generation
    time.

--- dokuforge/exportformatter.py
+++ dokuforge/exportformatter.py
@@ -23,3 +23,53 @@ class TeXFormatter(BaseFormatter):
     handle_list = u"\\begin{itemize}\n%s\n\end{itemize}".__mod__
     handle_item = u"\\item %s".__mod__
     handle_Dollar = u"%.0\\$%".__mod__
+
+    def generateoutput(self):
+        """
+        Take self.tree and generate an output string.
+
+        This method adds all the bells and whistles the exporter has.
+
+        This uses the handle_* methods to recursively transform the
+        tree. The escaping is done with escapemap and controlled by
+        escapeexceptions.
+        """
+        output = u""
+        for x in self.tree.tree:
+            value = self.supergenerateoutput(x, self.tree, ["root"])
+            try:
+                handler = getattr(self, "handle_%s" % x.ident)
+            except AttributeError:
+                pass
+            else:
+                value = handler(value)
+            output += value
+        return output
+
+    def supergenerateoutput(self, tree, neighbours, context, escape = True):
+        if isinstance(tree, ParseLeaf):
+            data = tree.data
+            try:
+                handler = getattr(self, "handle_%s" % tree.ident)
+            except AttributeError:
+                pass
+            else:
+                data = handler(data)
+            if escape:
+                return data.translate(self.escapemap)
+            return data
+        if tree.ident in self.escapeexceptions:
+            escape=False
+        output = u""
+        context.append(tree.ident)
+        for x in tree.tree:
+            value = self.supergenerateoutput(x, tree.tree, context, escape)
+            try:
+                handler = getattr(self, "handle_%s" % x.ident)
+            except AttributeError:
+                pass
+            else:
+                value = handler(value)
+            output += value
+        context.pop()
+        return output
