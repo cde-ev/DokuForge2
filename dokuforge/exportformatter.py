fcb031fe3d84b087d2676d1add972196b6399bbd
Markus Oehme <veelai.1764@googlemail.com>
2011-11-29 18:44:19 +0100

    Use Context class to unify the lookahead.

--- dokuforge/exportformatter.py
+++ dokuforge/exportformatter.py
@@ -13,72 +13,81 @@
 from dokuforge.baseformatter import BaseFormatter
 from dokuforge.parser import ParseLeaf
 
-def incontext(context, query):
-    return query in context
-
 whitelist = ["pi"]
 
 abbreviations = [["z", "B"], ["vgl"]]
 
-def lookleafdata(leaf, neighbours, n=1):
-    index = neighbours.index(leaf)
-    try:
-        if not isinstance(neighbours[index+n], ParseLeaf):
-            return None
-        return neighbours[index+n].data
-    except IndexError:
-        return None
+class Context:
+    def __init__(self, leaf, neighbours, environ):
+        self.leaf = leaf
+        self.neighbours = neighbours
+        self.environ = environ
+        self.index = neighbours.index(leaf)
 
-def lookleaftype(leaf, neighbours, n=1):
-    index = neighbours.index(leaf)
-    try:
-        if not isinstance(neighbours[index+n], ParseLeaf):
-            return None
-        return neighbours[index+n].ident
-    except IndexError:
-        return None
 
-def comparedata(leaf, neighbours, data):
-    for i in len(data):
-        if not lookleafdata(leaf, neighbours, i+1) == data[i]:
-            return False
-    return True
-
-def checkabbrev(abbrev, leaf, neighbours):
-    pos = 0
-    for x in abbrev:
-        if not lookleafdata(leaf, neighbours, pos) == x and \
-            lookleafdata(leaf, neighbours, pos + 1) == u'.':
-            return False
-        pos += 2
-        if lookleaftype(leaf,neighbours, pos) == "Whitespace":
-            pos += 1
-    return True
-
-def countabbrev(abbrev, leaf, neighbours):
-    pos = 0
-    for x in abbrev:
-        if not lookleafdata(leaf, neighbours, pos) == x and \
-            lookleafdata(leaf, neighbours, pos + 1) == u'.':
-            return 0
-        pos += 2
-        if lookleaftype(leaf,neighbours, pos) == "Whitespace":
-            pos += 1
-    return pos
-
-def scanfordash(leaf, neighbours):
-    pos = 3
-    if lookleafdata(leaf, neighbours, pos) == u'.' or \
-        lookleafdata(leaf, neighbours, pos + 1) == u'.' or \
-        lookleafdata(leaf, neighbours, pos + 2) == u'.':
+    def inenviron(self, query):
+        for x in query:
+            if x in self.environ:
+                return True
         return False
-    while not lookleafdata(leaf, neighbours, pos + 2) == u'.':
-        if lookleafdata(leaf, neighbours, pos) == u' ' and \
-            lookleafdata(leaf, neighbours, pos + 1) == u'-' and \
-            lookleafdata(leaf, neighbours, pos + 2) == u'-':
-            return True
-        pos += 1
-    return False
+
+
+    def lookleafdata(self, n=1):
+        try:
+            if not isinstance(self.neighbours[self.index+n], ParseLeaf):
+                return None
+            return self.neighbours[self.index+n].data
+        except IndexError:
+            return None
+
+    def lookleaftype(self, n=1):
+        try:
+            if not isinstance(self.neighbours[self.index+n], ParseLeaf):
+                return None
+            return self.neighbours[self.index+n].ident
+        except IndexError:
+            return None
+
+    def comparedata(self, data):
+        for i in range(len(data)):
+            if not self.lookleafdata(i+1) == data[i]:
+                return False
+        return True
+
+    def checkabbrev(self, abbrev):
+        pos = 0
+        for x in abbrev:
+            if not self.lookleafdata(pos) == x and \
+                self.lookleafdata(pos + 1) == u'.':
+                return False
+            pos += 2
+            if self.lookleaftype(pos) == "Whitespace":
+                pos += 1
+        return True
+
+    def countabbrev(self, abbrev):
+        pos = 0
+        for x in abbrev:
+            if not self.lookleafdata(pos) == x and \
+                self.lookleafdata(pos + 1) == u'.':
+                return 0
+            pos += 2
+            if self.lookleaftype(pos) == "Whitespace":
+                pos += 1
+        return pos
+
+    def scanfordash(self, pos=3):
+        if self.lookleafdata(pos) == u'.' or \
+            self.lookleafdata(pos + 1) == u'.' or \
+            self.lookleafdata(pos + 2) == u'.':
+            return False
+        while not self.lookleafdata(pos + 2) == u'.':
+            if self.lookleaftype(pos) == "Whitespace" and \
+                self.lookleafdata(pos + 1) == u'-' and \
+                self.lookleafdata(pos + 2) == u'-':
+                return True
+            pos += 1
+        return False
 
 
 class TeXFormatter(BaseFormatter):
@@ -114,52 +123,52 @@ class TeXFormatter(BaseFormatter):
         else:
             return "{%s}" % data
 
-    def advanced_handle_Backslash(self, leaf, neighbours, context):
-        if incontext(context, "ednote"):
-            return (u'\\', 0)
-        elif ( incontext(context, "inlinemath") or \
-               incontext(context, "displaymath")) and \
-            lookleafdata(leaf, neighbours) in whitelist:
-            return (u'\\' + lookleafdata(leaf, neighbours), 1)
-        elif ( incontext(context, "inlinemath") or \
-               incontext(context, "displaymath")) and \
-            lookleaftype(leaf, neighbours) == "Backslash":
-            if lookleaftype(leaf, neighbours, 2) == "Newline":
+    def advanced_handle_Backslash(self, leaf, context):
+        if context.inenviron(["inlinemath", "displaymath"]) and \
+            context.lookleafdata() in whitelist:
+            return (u'\\' + context.lookleafdata(), 1)
+        elif context.inenviron(["inlinemath", "displaymath"]) and \
+            context.lookleaftype() == "Backslash":
+            if context.lookleaftype(2) == "Newline":
                 return (u'\\\\', 1)
             else:
                 return (u'\\\\\n', 1)
         else:
-            if lookleaftype(leaf, neighbours) == "Backslash":
+            if context.lookleaftype() == "Backslash":
                 return (u'\\@\\forbidden\\newline ', 1)
-            elif lookleaftype(leaf, neighbours) == "Word":
+            elif context.lookleaftype() == "Word":
                 return (u'\\@\\forbidden\\', 0)
             else:
                 return (u'\\@\\forbidden\\ ', 0)
 
-    def advanced_handle_Word(self, leaf, neighbours, context):
-        if lookleafdata(leaf, neighbours) == u'.' and \
-            not (incontext(context, "inlinemath") or \
-                 incontext(context, "displaymath"))  :
+    def advanced_handle_Word(self, leaf, context):
+        if context.lookleafdata() == u'.' and \
+            not context.inenviron(["inlinemath", "displaymath"]):
             for abbrev in abbreviations:
-                if checkabbrev(abbrev, leaf, neighbours):
+                if context.checkabbrev(abbrev):
                     return(u'\\@' + u'.\\,'.join(abbrev) + u'.',
-                           countabbrev(abbrev, leaf, neighbours))
+                           context.countabbrev(abbrev))
             return (leaf.data, 0)
         else:
             return (leaf.data, 0)
 
-    def advanced_handle_Token(self, leaf, neighbours, context):
-        if leaf.data == u'.':
-            ## reset number of seen dashes at every full stop
-            self.dashesseen = 0
-            return (u'.', 0)
+    def advanced_handle_Token(self, leaf, context):
+        if context.inenviron(["ednote", "nestedednote"]):
+            return (leaf.data, 0)
+        elif leaf.data == u'.':
+            if context.comparedata([u'.', u'.']):
+                return (u'\\a\\ldots{}', 2)
+            else:
+                ## reset number of seen dashes at every full stop
+                self.dashesseen = 0
+                return (u'.', 0)
         elif leaf.data == u'-':
-            if lookleafdata(leaf, neighbours) == u'-':
-                if not lookleaftype(leaf, neighbours, 2) == "Number":
+            if context.lookleafdata() == u'-':
+                if not context.lookleaftype(2) == "Number":
                     if self.dashesseen % 2 == 1:
                         self.dashesseen += 1
                         return (u'\@~--', 1)
-                    elif lookleaftype(leaf, neighbours, 2) == "Whitespace":
+                    elif context.lookleaftype(2) == "Whitespace":
                         self.dashesseen += 1
                         return (u'\@--~', 2)
                     else:
@@ -172,13 +181,13 @@ class TeXFormatter(BaseFormatter):
         elif leaf.data == u'^':
             ## prevent '^^'
             skips = 0
-            while lookleafdata(leaf, neighbours, skips+1) == u'^':
+            while context.lookleafdata(skips+1) == u'^':
                 skips += 1
             return (u'^', skips)
         else:
             return (leaf.data, 0)
 
-    def advanced_handle_Number(self, leaf, neighbours, context):
+    def advanced_handle_Number(self, leaf, context):
         number = u''
         if len(leaf.data) < 5:
             number = leaf.data
@@ -193,10 +202,9 @@ class TeXFormatter(BaseFormatter):
             number = number[:-2]
         return (number, 0)
 
-    def advanced_handle_Whitespace(self, leaf, neighbours, context):
-        if lookleafdata(leaf, neighbours, 1) == '-' and \
-            lookleafdata(leaf, neighbours, 2) == '-':
-            if self.dashesseen % 2 == 1 or not scanfordash(leaf, neighbours):
+    def advanced_handle_Whitespace(self, leaf, context):
+        if context.comparedata([u'-', u'-']):
+            if self.dashesseen % 2 == 1 or not context.scanfordash():
                 self.dashesseen += 1
                 return (u'\@~--', 2)
             else:
@@ -220,7 +228,8 @@ class TeXFormatter(BaseFormatter):
             assert x.ident in ("paragraph", "heading", "subheading", "authors",
                                "displaymath", "list", "ednote", "Newpar",
                                "Newline")
-            value, _ = self.advancedgenerateoutput(x, self.tree, ["root"])
+            value, _ = self.advancedgenerateoutput(x, Context(x, self.tree.tree,
+                                                              ["root"]))
             try:
                 handler = getattr(self, "handle_%s" % x.ident)
             except AttributeError:
@@ -230,7 +239,7 @@ class TeXFormatter(BaseFormatter):
             output += value
         return output
 
-    def advancedgenerateoutput(self, tree, neighbours, context):
+    def advancedgenerateoutput(self, tree, context):
         if isinstance(tree, ParseLeaf):
             data = tree.data
             skips = 0
@@ -241,7 +250,7 @@ class TeXFormatter(BaseFormatter):
             except AttributeError:
                 pass
             else:
-                data, skips = handler(tree, neighbours, context)
+                data, skips = handler(tree, context)
             if handler is None:
                 ## if no advanced handler, try normal handler
                 try:
@@ -253,14 +262,15 @@ class TeXFormatter(BaseFormatter):
             return (data, skips)
         ## now we have a ParseTree
         output = u""
-        context.append(tree.ident)
         skips = 0
+        context.environ.append(tree.ident)
         for x in tree.tree:
             ## skip nodes, allready processed earlier in the loop
             if skips > 0:
                 skips -= 1
                 continue
-            value, skips = self.advancedgenerateoutput(x, tree.tree, context)
+            value, skips = self.advancedgenerateoutput(x, Context(x, tree.tree,
+                                                                  context.environ))
             try:
                 handler = getattr(self, "handle_%s" % x.ident)
             except AttributeError:
@@ -268,5 +278,5 @@ class TeXFormatter(BaseFormatter):
             else:
                 value = handler(value)
             output += value
-        context.pop()
+        context.environ.pop()
         return (output, 0)
