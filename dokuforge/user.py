2c0cc3d2cb8a4c08e5293d22ab864e6e363e89b8
Markus Oehme <veelai.1764@googlemail.com>
2011-12-12 01:00:02 +0100

    Tweak specification.

--- dokuforge/user.py
+++ dokuforge/user.py
@@ -50,8 +50,7 @@ class User:
             academy but does not grant any recursive priveleges (in contrast
             to akademie_read_* which allows to read all courses). The
             metadata privelege grants the ability to modify academy/course
-            titles and in conjunction with write the ability to create new
-            courses.
+            titles and the ability to create new courses.
          - gruppe_x_y --
             x in {read, write, show, metadata}, y gruppenname
 
