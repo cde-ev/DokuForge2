474b7a24aceb526f82b9e8326cd6de7b17528122
Klaus Aehlig <aehlig@hilbert.linta.de>
2012-04-15 17:30:34 +0200

    Completed framework, and added first feature: paragraph

--- dokuforge/parser.py
+++ dokuforge/parser.py
@@ -1,3 +1,4 @@
+import re
 
 class Linegroup:
     """
@@ -13,9 +14,11 @@ class Linegroup:
     item-entries cann be grouped to an itemization environment, thus
     yielding a parse tree of the whole dokument.
     """
-    def __init__(self):
+    def __init__(self, initialline=None):
         self.lines = []
         self.printname = "abstract linegroup"
+        if initialline is not None:
+            self.appendline(initialline)
 
     def startshere(self, line, after=None):
         """
@@ -25,22 +28,40 @@ class Linegroup:
         """
         return False
 
+    def enforcecontinuation(self, line):
+        """
+        Decide if this group enforces that the next line belongs to
+        the current group, even though it could start a new group.
+        Useful for ednotes or similar greedy groups.
+        """
+        return False
+
     def appendline(self, line):
         self.lines.append(line)
 
     def debug(self):
         return (self.printname, self.lines)
 
+
+def isemptyline(line):
+    return re.match('^[ \t]*$', line)
+
 class Paragraph(Linegroup):
     """
     A standard paragraph. This hopefully should be the most common
     line group in a document.
     """
-
-    def __init__(self):
-        Linegroup.__init__(self)
+    
+    def __init__(self, initialline=None):
+        Linegroup.__init__(self, initialline=initialline)
         self.printname = "Paragraph"
 
+    def appendline(self, line):
+        if not isemptyline(line):
+            Linegroup.appendline(self, line)
+
+    def startshere(self, line):
+        return isemptyline(line)
 
 
 def grouplines(lines, supportedgroups):
@@ -48,7 +69,42 @@ def grouplines(lines, supportedgroups):
     Given a list of lines and a list of Linegroup to support, group
     lines accordingly.
 
-    The grouping is done based on the startshere function provided
-    by the supported linegroups.
+    The grouping is done based on the startshere and enforcecontinuatuion
+    functions provided by the supported linegroups.
     """
-    pass ## FIXME: continue
+    current = Paragraph()
+    groups = []
+    for line in lines:
+        if current.enforcecontinuation(line):
+            current.appendline(line)
+        else:
+            handled = False
+            for linegroup in supportedgroups:
+                if not handled:
+                    if linegroup.startshere(line):
+                        groups.append(current)
+                        current = linegroup.__class__(initialline=line)
+                        handled = True
+            if not handled:
+                current.appendline(line)
+    groups.append(current)
+    return groups
+
+
+if __name__ == "__main__":
+    example = """
+[Ueberschrift]
+(Autor)
+
+Hier ist ein
+Paragrpah ueber 3
+Zeilen.
+
+Und ein weiterer
+Absatz.
+(Man beachte, dass diese Klammer
+keine Autorenangabe beinhaltet)
+"""
+    features = [Paragraph()]
+    groups = grouplines(example.splitlines(), features)
+    print [g.debug() for g in groups]
