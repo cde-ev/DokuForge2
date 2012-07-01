b32653de6f4e7f45055a712345002654f47d573d
Markus Oehme <mo@wurzel.org>
2012-07-01 19:58:39 +0200

    Fix merge accident, this must have slipped through

--- dokuforge/application.py
+++ dokuforge/application.py
@@ -1470,6 +1470,7 @@ class Application:
             academy=theacademy.view(),
             course=thecourse.view(),
             page=thepage,
+            commit = thecourse.getcommit(thepage),
             content=parsed.toHtml(),
             saved=saved,
             blobs=[thecourse.viewblob(i) for i in thecourse.listblobs(thepage)])
