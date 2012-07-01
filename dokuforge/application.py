37c7dc19a63106954925d9d214bfd507c251175b
Markus Oehme <mo@wurzel.org>
2012-07-01 20:16:56 +0200

    Fix blob exceptions and config checks which were mangled during the merge.

--- dokuforge/application.py
+++ dokuforge/application.py
@@ -1089,17 +1089,23 @@ class Application:
         try:
             c.attachblob(page, usercontent, comment=usercomment,
                          label=userlabel, user=rs.user.name)
-        except CheckError as error:
-            usercontent.filename = u"einedatei.dat"
-            if error.message == u"Dateiname nicht wohlgeformt!":
+        except common.InvalidBlobFilename as error:
+            usercontent.filename = common.sanitizeBlobFilename(
+                usercontent.filename)
+            try:
                 blob = c.attachblob(page, usercontent, comment=usercomment,
                                 label=userlabel, user=rs.user.name)
+            except CheckError:
+                ## this case should never happen
+                ## (except manually crafted POSTs)
+                return self.render_addblob(rs, aca, c, page)
             else:
-                blob = c.attachblob(page, usercontent, comment=u"Bildunterschrift",
-                                    label=u"somefig", user=rs.user.name)
-            return self.render_editblob(rs, aca, c, page, blob, ok=False,
-                                       error=error)
-        return self.render_show(rs, aca, c, page)
+                return self.render_editblob(rs, aca, c, page, blob, ok=False,
+                                            error=error)
+        except CheckError:
+            return self.render_addblob(rs, aca, c, page) # also shouldn't happen
+        else:
+            return self.render_show(rs, aca, c, page)
 
     def do_save(self, rs, academy = None, course = None, page = None):
         """
@@ -1228,22 +1234,6 @@ class Application:
             return werkzeug.exceptions.Forbidden()
         return self.do_file(rs, self.userdb.storage, "admin.html")
 
-    def tryConfigParser(self, content):
-        """
-        Try parsing the supplied content with ConfigParser. If this fails
-        raise a CheckError saying so.
-
-        @type content: unicode
-        """
-        assert isinstance(content, unicode)
-        try:
-            config = ConfigParser.SafeConfigParser()
-            config.readfp(StringIO(content.encode("utf8")))
-        except ConfigParser.ParsingError as err:
-            raise CheckError(u"Es ist ein Parser Error aufgetreten!",
-                             u"Der Fehler lautetete: " + err.message + \
-                             u". Bitte korrigiere ihn und speichere erneut.")
-
     def do_adminsave(self, rs):
         """
         @type rs: RequestState
@@ -1252,7 +1242,7 @@ class Application:
         if not rs.user.isAdmin():
             return werkzeug.exceptions.Forbidden()
         return self.do_filesave(rs, self.userdb.storage, "admin.html",
-                                checkhook = self.tryConfigParser,
+                                checkhook = common.validateUserConfig,
                                 savehook = self.userdb.load)
 
     def do_groups(self, rs):
@@ -1272,7 +1262,7 @@ class Application:
         if not rs.user.isSuperAdmin():
             return werkzeug.exceptions.Forbidden()
         return self.do_filesave(rs, self.groupstore, "groups.html",
-                                checkhook = self.tryConfigParser)
+                                checkhook = common.validateGroupConfig)
 
     ### here come the renderer
 
--- dokuforge/common.py
+++ dokuforge/common.py
@@ -129,6 +129,9 @@ def validateBlobComment(comment):
         raise CheckError(u"Keine Bildunterschrift gefunden!",
                          u"Bitte eine Bildunterschrift eingeben und erneut versuchen.")
 
+class InvalidBlobFilename(CheckError):
+    pass
+
 def validateBlobFilename(filename):
     """
     check whether a filename for a blob is valid. This means matching a certain
@@ -136,7 +139,7 @@ def validateBlobFilename(filename):
 
     @type filename: str
     @param filename: filename to check
-    @raises CheckError:
+    @raises InvalidBlobFilename:
     """
     assert isinstance(filename, str)
     if re.match('^[a-zA-Z0-9][-a-zA-Z0-9_.]{1,200}[a-zA-Z0-9]$', filename) is None:
