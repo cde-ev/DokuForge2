cde-ev/DokuForge2

commit 9de4b9d8042a9b16dd8e27d739a3c41f1c2e3168

Minimal whitespace adjustment.
Allow tabulators after enumeration and strip the whitespace between
enumeration and content of an item.

dokuforge/parser.py
@@ -1017,14 +1017,14 @@ class EnumerateItem(Linegroup):
     
     @classmethod
     def startshere(self, line, after=None):
-        return re.match('^[0-9]+\. ', line)
+        return re.match('^[0-9]+\.[ \t]', line)
 
     def parse(self):
         if len(self.lines) < 1:
             return PItem(defaultInnerParse(self.lines), number="1")
         firstline = self.lines[0]
         number = "1"
-        m = re.match('^([0-9]+)\.(.*)$', firstline)
+        m = re.match('^([0-9]+)\.[ \t]+(.*)$', firstline)
         if m is not None:
             number, firstline = m.group(1,2)
         withcleanedfirstline = [firstline]

commit 1705f4ed96091791dc06160cbf6091ac956e7cc2

Fix parser regression introduced by 9de4b9d8042a9b16dd8e27d739a3c41f1c2e3168
...by fixing the canonical df representation of an enumerate item.

dokuforge/parser.py
@@ -491,7 +491,7 @@ class PItem(PTree):
         if self.number is None:
             return '\n-' + self.it.toDF()
         else:
-            return '\n' + self.number + '.' + self.it.toDF()
+            return '\n' + self.number + '. ' + self.it.toDF()
 
 class Chargroup:
     """