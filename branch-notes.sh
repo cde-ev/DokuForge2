1f3077d31a4a741fcdbb2a806475b62815563be7
Markus Oehme <veelai.1764@googlemail.com>
2013-04-30 09:20:08 +0200

    Improve script
    Use more appropriate git options. There should be no bash-specific things,
    so use any POSIX-shell.

--- branch-notes.sh
+++ branch-notes.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 for BRANCH in `git branch -r | grep -v HEAD`
 do
-        git notes show ` git log ${BRANCH}  | head -n1 | cut -d ' ' -f  2 ` &> /dev/null
+        git notes show ` git --no-pager log -1 --format=%H ${BRANCH} ` &> /dev/null
         if [ $? -eq 0 ]; then
                 printf "$BRANCH\n"
-                git notes show ` git log ${BRANCH}  | head -n1 | cut -d ' ' -f  2 ` | cat
+                git --no-pager notes show ` git --no-pager log -1 --format=%H ${BRANCH} `
                 printf -- "------------------------------------------------------------\n"
         fi
 done
