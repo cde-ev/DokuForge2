f26c044425a306e0ad380dc71724c649adcd1cd9
Helmut Grohne <helmut@subdivi.de>
2013-05-02 13:58:55 +0200

    branch-notes: do not use bash features with /bin/sh

--- branch-notes.sh
+++ branch-notes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for BRANCH in `git branch -r | grep -v HEAD`
 do
-        git notes show ${BRANCH} &> /dev/null
+        git notes show ${BRANCH} >/dev/null 2>&1
         if [ $? -eq 0 ]; then
                 printf "$BRANCH\n"
                 git --no-pager notes show ${BRANCH}
