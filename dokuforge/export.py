ec738c8d19f66c8716c5aef9f4a602272c0a1a10
Ole Schwen <mail@oleschwen.de>
2015-11-08 00:54:38 +0100

    first version of export to tar file need to improve arguments, do not necessarily want to keep writing to tar files

--- dokuforge/export.py
+++ dokuforge/export.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 """
-Usage: python -m dokuforge.export academy_or_course_directory
+Usage: python -m dokuforge.export df2_academy_directory dokuforge-export-static_directory academy_name
 """
 
 import os.path
 import sys
 
-from dokuforge.course import Course
-from dokuforge.parser import dfLineGroupParser
+from dokuforge.course  import Course
+from dokuforge.parser  import dfLineGroupParser
+from dokuforge.academy import Academy
+from dokuforge.common  import TarWriter
 
+from tarfile import TarFile
+
+'''
+# currently not needed
 class PseudoTarWriter:
     def __init__(self, directory):
         self.directory = directory
@@ -26,19 +32,35 @@ def process_course(directory):
     c = Course(directory)
     for _ in c.texExportIterator(tw):
         pass
+'''
 
-def process(directory):
-    if os.path.exists(os.path.join(directory, "Index,v")):
-        process_course(directory)
-    else:
-        for elem in os.listdir(directory):
-            entry = os.path.join(directory, elem)
-            if os.path.isdir(entry):
-                process(entry)
+def process(academiesdir,staticexportdir,academyname):
+    academy = Academy ( os.path.join(academiesdir,academyname), [] )
+
+    # duplicate code from application.py
+    prefixdir = b"texexport_" + academyname
+
+    def export_iterator(aca, staticexpdir, prefix):
+        tarwriter = TarWriter(gzip=False)
+        tarwriter.pushd(prefix)
+        for chunk in aca.texExportIterator(tarwriter,
+                                           static=staticexpdir):
+            yield chunk
+        tarwriter.popd()
+        yield tarwriter.close()
+
+    tarGenerator = export_iterator(academy,staticexportdir,prefixdir)
+
+    outputfile = open ( prefixdir+'.tar', 'w' )
+
+    for chunk in tarGenerator:
+        outputfile.write(chunk)
 
 def main():
-    basedir = sys.argv[1]
-    process(basedir)
+    academiesdir    = sys.argv[1]
+    staticexportdir = sys.argv[2]
+    academyname     = sys.argv[3]
+    process(academiesdir,staticexportdir,academyname)
 
 if __name__ == "__main__":
     main()
