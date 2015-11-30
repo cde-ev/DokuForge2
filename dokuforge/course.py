8d6c3328694f5c7b3ae54788dcc0137d6c99df3f
Ole Schwen <mail@oleschwen.de>
2015-11-30 21:54:53 +0100

    leave a comment in the exported .tex file if file name was changed by the exporter

--- dokuforge/course.py
+++ dokuforge/course.py
@@ -636,6 +636,8 @@ class Course(StorageDir):
                     (u"\\includegraphics" +
                      u"[height=12\\baselineskip]{%s/blob_%d_%s}\n") % \
                     (self.name, b, fileName)
+                if fileName != blob['filename']:
+                    tex += (u"%% Original-Dateiname: %s\n" % blob['filename'])
                 if fileName.endswith((".png", ".jpg", ".pdf")):
                     tex += includegraphics
                 else:
