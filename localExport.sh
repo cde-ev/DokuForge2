1fc2281434996842c8039dd92804e69e241d3e24
Ole Schwen <mail@oleschwen.de>
2016-11-05 18:58:25 +0100

    made export script add input to the export

--- localExport.sh
+++ localExport.sh
@@ -15,6 +15,7 @@ EXPORTSTATICCLEANDIR=`mktemp -d --dry-run`
 EXPORTEDACAFILENAME=texexport_$ACANAME.tar
 EXPORTEDACADIR=$EXPORTDIR/texexport_$ACANAME
 COMMITID=`git show -s --format=%H`
+CURRENTDIR=`pwd`
 
 echo $EXPORTDIR
 echo $ACANAME
@@ -50,6 +51,47 @@ echo "Done."
 # unpack export
 tar -C $EXPORTDIR -xvf $EXPORTEDACAFILENAME
 
+# add input to export
+# for i in `find $DFACADIR -name "blob*,v"` `find $DFACADIR -name "Index,v"` `find $DFACADIR -name "isDeleted,v"` `find $DFACADIR -name "nextpage,v"` `find $DFACADIR -name "nextblob,v"`
+# do
+#     rm $i
+# done
+
+cd $DFACADIR
+rm *,v
+for d in *
+do
+    echo $d
+    cd $d
+    for i in *,v
+    do
+        echo $i
+        co $i
+        rm -f $i
+    done
+    shopt -s nullglob
+    for i in title page* blob*.filename blob*.comment
+    do
+        echo $i >> input.txt
+        cat $i >> input.txt
+        echo >> input.txt
+        rm -f $i
+    done
+    cd ..
+done
+
+for d in *
+do
+    cd $d
+    if [ ! -d $EXPORTEDACADIR/$d ]; then
+        mkdir $EXPORTEDACADIR/$d
+    fi
+    cp input.txt $EXPORTEDACADIR/$d/
+    cd ..
+done
+
+cd $CURRENTDIR
+
 # clean up files created during exporting
 rm -rf $DFACADIR $EXPORTEDACAFILENAME $EXPORTSTATICCLEANDIR
 
@@ -61,4 +103,3 @@ echo "same exporter semantics, keep the following version string"  >> $EXPORTEDA
 echo "for your reference."                                         >> $EXPORTEDACADIR/WARNING
 echo >> $EXPORTEDACADIR/WARNING
 echo $COMMITID >> $EXPORTEDACADIR/WARNING
-
